53d4726323bc2b6d4782ea60bc979074434d1950
Alessandro Monticelli <alexmonticelli02@gmail.com>
2021-11-03 21:27:36 +0100

    Update game.c
    Fix bug minori. todo: Fix test

--- elaborato6/game.c
+++ elaborato6/game.c
@@ -27,6 +27,9 @@ typedef struct{
 palla ball;
 paletta pad1, pad2;
 
+/*Riporta la pallina alla posizione iniziale*/
+static void reset_ball();
+
 void setup_game(int height, int width, 
                 struct position ball_pos, struct position ball_dir,
                 struct position pad1_pos, struct position pad2_pos, int pad_len){
@@ -63,81 +66,110 @@ void setup_game(int height, int width,
 
 /* Moves pad1 one position up. */
 void move_pad1_up(void){
-	if(pad1.p.y > 0)
+	if((0 < pad1.p.y) && ((((ball.p.x != pad1.p.x + -1 && 
+		(ball.p.x != pad1.p.x)) && (ball.p.x != pad1.p.x + 1)) ||
+      	(ball.p.y !=pad1.p.y + -1))))
+
 		pad1.p.y += UP;
+	
+	return;
 }
 
 /* Moves pad2 one poisiton up */
 void move_pad2_up(void){
-	if(pad2.p.y > 0)
+	if((0 < pad2.p.y) && ((((ball.p.x != pad2.p.x + -1 && (ball.p.x != pad2.p.x)) && 
+	 	(ball.p.x != pad2.p.x + -2)) ||
+      	(ball.p.y != pad2.p.y + -1))))
+		
 		pad2.p.y += UP;
+
+	return;
 }
 
 /* Moves pad1 one position down. */
 void move_pad1_down(void){
-	if(pad1.p.y + pad1.len < pad1.max_height)
+	if((pad1.p.y + -1 + pad1.len < pad1.max_height) &&
+     	((((ball.p.x != pad1.p.x + -1 && (ball.p.x != pad1.p.x))
+	 	&& (ball.p.x != pad1.p.x + 1)) ||
+      	(ball.p.y != pad1.p.y + pad1.len))))
+
 		pad1.p.y += DOWN;
+	
+	return;
 }
 
 /* Moves pad2 one position down. */
 void move_pad2_down(void){
-	if(pad2.p.y + pad2.len < pad2.max_height)
+	if((pad2.p.y + -1 + pad2.len < pad2.max_height) &&
+     ((((ball.p.x != pad2.p.x + -1 && (ball.p.x != pad2.p.x)) && (ball.p.x != pad2.p.x + -2)) ||
+      (ball.p.y != pad2.p.y + pad2.len))))
+
 		pad2.p.y += DOWN;
+	
+	return;
 }
 
 /* Moves the ball in the current direction */
 void move_ball(void){
 
-    if(ball.p.x <= 0){
-        pad2.score += 1;
-
-        ball.p.x = ball.init_pos.x;
-		ball.p.y = ball.init_pos.y;
-		ball.d.x = ball.init_dir.x;
-		ball.d.y = ball.init_dir.y;
+    if ((ball.p.x == 0) || (ball.p.x == ball.max_width -1)) {
+    if (ball.p.x == 0) {
+      pad2.score = pad2.score + 1;
     }
-    if(ball.p.x >= ball.max_width){
-        pad1.score += 1;
-
-        ball.p.x = ball.init_pos.x;
-		ball.p.y = ball.init_pos.y;
-		ball.d.x = ball.init_dir.x;
-		ball.d.y = ball.init_dir.y;
+    else {
+      pad1.score = pad1.score + 1;
     }
+    reset_ball();
+  }
+
     /*Tocco pieno pad1*/
-	if(ball.p.x == (pad1.p.x + 1) && (ball.p.y >= pad1.p.y && ball.p.y <= pad1.p.y + pad1.len)){
+	if(((((ball.p.x == pad1.p.x + -1) || ((ball.p.x == pad1.p.x || (ball.p.x == pad1.p.x + 1)))) &&
+         (ball.p.y == pad1.p.y + -1)) ||
+        ((((ball.p.x == pad1.p.x + -1 || (ball.p.x == pad1.p.x)) || (ball.p.x == pad1.p.x + 1)) &&
+         (ball.p.y == pad1.p.y + pad1.len)))) ||
+       (((ball.p.x == pad1.p.x + 1 && (pad1.p.y <= ball.p.y)) && (ball.p.y <= pad1.p.y + -1 + pad1.len)))){
 		ball.d.x = RIGHT;
 	}
     /*Tocco pieno pad2*/
-	if(ball.p.x == (pad2.p.x - 2) && (ball.p.y >= pad2.p.y && ball.p.y <= pad2.p.y + pad2.len)){
+	if((((((ball.p.x == pad2.p.x + -1) || (ball.p.x == pad2.p.x)) || (ball.p.x == pad2.p.x + -2)) &&
+         (ball.p.y == pad2.p.y + -1)) ||
+        ((((ball.p.x == pad2.p.x + -1 || (ball.p.x == pad2.p.x)) || (ball.p.x == pad2.p.x + -2)) &&
+         (ball.p.y == pad2.p.y + pad2.len)))) ||
+       (((ball.p.x == pad2.p.x + -2 && (pad2.p.y <= ball.p.y)) && (ball.p.y <= pad2.p.y + -1 + pad2.len)))){
 		ball.d.x = LEFT;
 	}
     /*Tocco sopra pad1*/
-	if((ball.p.x == (pad1.p.x + 2) && (ball.p.y == pad1.p.y - 1)) || (ball.p.x == (pad1.p.x + 1) && (ball.p.y == pad1.p.y))){
-		ball.d.y = UP;
-		ball.d.x = RIGHT;
+	if(ball.p.y == pad1.p.y || ball.p.y == pad1.p.y + 1 || ball.p.y == pad2.p.y || ball.p.y == pad2.p.y + 1){
+		if((((ball.p.x == pad1.p.x + -1) || (ball.p.x == pad1.p.x)) || (ball.p.x == pad1.p.x + 1)) &&
+			(ball.p.y == pad1.p.y + pad1.len)){
+			ball.d.y = UP;
+			ball.d.x = RIGHT;
+		}
+		/*Tocco sopra pad2*/
+		if(((ball.p.x == pad2.p.x + -1 || (ball.p.x == pad2.p.x)) || (ball.p.x == pad2.p.x + -2)) &&
+			(ball.p.y == pad2.p.y + pad2.len)){
+			ball.d.y = DOWN;
+			ball.d.x = RIGHT;
+		}
+		/*Tocco sotto pad1*/
+		if((((ball.p.x == pad1.p.x + -1) || (ball.p.x == pad1.p.x)) || (ball.p.x == pad1.p.x + 1)) &&
+			(ball.p.y == pad1.p.y + -1)) {
+			ball.d.y = UP;
+			ball.d.x = LEFT;
+		}
+		/*Tocco sotto pad2*/
+		if((((ball.p.x == pad2.p.x + -1 || (ball.p.x == pad2.p.x)) || (ball.p.x == pad2.p.x + -2)) &&
+			(ball.p.y == pad2.p.y + -1))){
+			ball.d.y = DOWN;
+			ball.d.x = LEFT;
+		}
 	}
-    /*Tocco sotto pad1*/
-	if((ball.p.x == (pad1.p.x + 1) && (ball.p.y == pad1.p.y + pad1.len - 1)) || (ball.p.x == (pad1.p.x) && (ball.p.y == pad1.p.y + pad1.len))){
-		ball.d.y = DOWN;
-		ball.d.x = RIGHT;
-	}
-    /*Tocco sopra pad2*/
-	if((ball.p.x == (pad2.p.x - 2) && (ball.p.y == pad2.p.y - 1)) || (ball.p.x == (pad2.p.x - 1) && (ball.p.y == pad2.p.y))){
-		ball.d.y = UP;
-		ball.d.x = LEFT;
-	}
-    /*Tocco sotto pad2*/
-	if((ball.p.x == (pad2.p.x - 2) && (ball.p.y == pad2.p.y + pad2.len)) || (ball.p.x == (pad2.p.x - 1) && (ball.p.y == pad2.p.y + pad2.len))){
-		ball.d.y = DOWN;
-		ball.d.x = LEFT;
-	}
-	
 	/*Rimbalzo bordi*/
-	if(ball.p.y <= 0)
-        ball.d.y  = DOWN;
-    if(ball.p.y == ball.max_height) {
-        ball.d.y = UP;
+	if (ball.p.y == 0) {
+      ball.d.y = DOWN;
+    }
+    if (ball.p.y == ball.max_height) {
+      ball.d.y = UP;
     }
 	
     /* always used */
@@ -175,5 +207,11 @@ unsigned int get_pad2_score(void){
 	return pad2.score;
 }
 
+static void reset_ball(void){
+	ball.p.x = ball.init_pos.x;
+	ball.p.y = ball.init_pos.y;
+	ball.d.x = ball.init_dir.x;
+	ball.d.y = ball.init_dir.y;
+}
 
 #endif
